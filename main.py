700505ddabca698270ca06843661af591b48ec86
Yamil Serafin Cruz Ruiz <20210656@uthh.edu.mx>
2025-07-20 01:39:51 -0600

    Microservicio Primer Commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from recomender_service import RecomenderService
+
+app = FastAPI()
+
+@app.get('/')
+async def root():
+  return {
+    "message": "Sistema de Recomendación de Productos"
+  }
+
+@app.get('/recommend/{partNumber}')
+async def predict(partNumber: str, n_items: int = 5):
+  recomender = RecomenderService()
+  recomendations: list[str] = recomender.recomendation(partNumber, n_items)
+  
+  return {
+    "recomendations": recomendations
+}
---
+++ recomender_service.py
@@ -0,0 +1,31 @@
+import pandas as pd
+import joblib as jl
+from typing import Any
+
+class RecomenderService:
+  
+  path: str = './model/modelo_recomendacion.pkl'
+
+  def __init__(self):
+    self.model: pd.DataFrame = jl.load(self.path)
+    
+  def recomendation(self, product_code: str, n: int = 5) -> list[str]:
+    if not self._exists_product_code(product_code):
+      return []
+
+    relations: Any = self._get_relations(product_code, n)
+    
+    return self._get_recomendations(relations)
+       
+  def _get_relations(self, product_code: str, n: int = 5) -> Any:
+    return self.model[product_code].sort_values(ascending=False).head(n)
+  
+  def _get_recomendations(self, relations: Any ) -> list[str]:
+    recomendations: list = []
+    for product, value in relations.items():
+      recomendations.append(product)
+      
+    return recomendations
+  
+  def _exists_product_code(self, product_code: str) -> bool:
+    return product_code in self.model.columns
